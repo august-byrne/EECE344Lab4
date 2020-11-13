f66670418f839cf923c1376dc0e3f59366bff829
byrnea4 <byrnea4@wwu.edu>
2020-11-12 21:03:06 -0800

    Added ControlDisplayTask guts. Pre-testing commit.

--- abLab4Project/source/Lab4Main.c
+++ abLab4Project/source/Lab4Main.c
@@ -1,31 +1,34 @@
 /*******************************************************************************
 * EECE344 Lab 4 Code
-*	This program allows the user to do things
-* August Byrne, 11/6/2020
+*	This program allows the user to activate and deactivate an alarm tone
+* August Byrne, 11/12/2020
 *******************************************************************************/
 #include "MCUType.h"               /* Include header files                    */
-//#include "MemTest.h"
-#include "BasicIO.h"
+#include "MemTest.h"
+//#include "BasicIO.h"
 #include "K65TWR_ClkCfg.h"
 //#include "MK65F18.h"
 #include "K65TWR_GPIO.h"
 #include "Key.h"
-
+#include "LCD.h"
 #include "SysTickDelay.h"
 
 #define POLL_PERIOD 10
 
 void ControlDisplayTask(void);
 
-void main(void){
+static INT8U ALARM_STATE = 0;	//initial state is alarm off
+static INT8U PAST_STATE = 1;
 
+void main(void){
     K65TWR_BootClock();             /* Initialize MCU clocks                  */
     BIOOpen(BIO_BIT_RATE_115200);   /* Initialize Serial Port                 */
     SysTickDlyInit();
     GpioDBugBitsInit();
+    LcdDispInit();
+
     INT32U low_addr = 0x00000000;		//Received low address
     INT32U high_addr = 0x001FFFFF;		//Received high address
-
 	BIOPutStrg("CS: ");					//program start initial checksum
 	BIOOutHexWord(low_addr);
 	BIOPutStrg("-");
@@ -41,11 +44,34 @@ void main(void){
     	//AlarmWaveControlTask();
     	KeyTask();
     }
-
 }
 
 void ControlDisplayTask(void){
 	DB1_TURN_ON();
-
+	current_key = KeyScan();
+	switch (ALARM_STATE){
+	case 0:
+		if (PAST_STATE != ALARM_STATE){		//display "alarm off" on the lcd
+			LcdDispString("ALARM OFF");
+			PAST_STATE = ALARM_STATE;
+		}
+		if (current_key == 0x04){			//if a is pressed, set alarm state as on
+			ALARM_STATE = 1;
+		}
+											//alarm signal is off
+		break;
+	case 1:
+		if (PAST_STATE != ALARM_STATE){		//display "alarm on" on the lcd
+			LcdDispString("ALARM ON");
+			PAST_STATE = ALARM_STATE;
+		}
+		if (current_key == 0x10){			//if d is pressed, set alarm state as off
+			ALARM_STATE = 0;
+		}
+		//AlarmWaveControlTask();			//alarm signal is on
+		break;
+	default:
+		ALARM_STATE = 0;
+	}
 	DB1_TURN_OFF();
 }
